19fc833796cc7be6cf90886660df23e6f7d2c266
brianahart <bnh5et@virginia.edu>
2016-10-17 13:23:55 -0400

    Milestone 2

--- app/about_us/about_us.component.ts
+++
@@ -1,7 +0,0 @@
-import { Component }            from '@angular/core';
-
-@Component({
-    selector: 'aboutus-component',
-    templateUrl: 'app/about_us/about_us.component.html'
-})
-export class AboutUsComponent { }
