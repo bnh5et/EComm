fed3cc0a3305231aa1ce9ac2579e1fd2fbb3ae12
Hero Lee <Hero Lee>
2016-10-24 23:06:28 -0400

    Altered the database.

--- db.sql
+++ db.sql
@@ -16,6 +16,29 @@
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
+-- Table structure for table `textbook`
+--
+
+DROP TABLE IF EXISTS `textbook`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `textbook` (
+  `isbn` int(12) NOT NULL,
+  `subject` varchar(45) DEFAULT NULL,
+  PRIMARY KEY (`isbn`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `textbook`
+--
+
+LOCK TABLES `textbook` WRITE;
+/*!40000 ALTER TABLE `textbook` DISABLE KEYS */;
+/*!40000 ALTER TABLE `textbook` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
 -- Table structure for table `user`
 --
 
@@ -43,7 +66,7 @@ CREATE TABLE `user` (
 
 LOCK TABLES `user` WRITE;
 /*!40000 ALTER TABLE `user` DISABLE KEYS */;
-INSERT INTO `user` VALUES ('Woahnelly','Amber','Lee','ajl4cf@virginia.edu','','','MD',NULL,NULL,NULL,NULL),('turdymcturdface','Turdy','McTurdFace','turd@yahoo.com','','','VT',NULL,NULL,NULL,NULL);
+INSERT INTO `user` VALUES ('Woahnelly','Amber','Lee','ajl4cf@virginia.edu','','','MD',NULL,NULL,NULL,NULL),('turdymcturdface','Turdy','McTurdFace','turd@yahoo.com','','','VT',NULL,NULL,NULL,NULL),('alphamale','Joshua','Kelly','joshuakelly@gmail.com','Po Po','Girugamesh','VA',NULL,NULL,NULL,NULL),('alphamale','Joshua','Kelly','joshuakelly@gmail.com','Po Po','Girugamesh','VA',NULL,NULL,NULL,NULL),('alphamale','WA','WA','joshuakelly@gmail.com','Po Po','Girugamesh','VA',NULL,NULL,NULL,NULL);
 /*!40000 ALTER TABLE `user` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -56,4 +79,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2016-10-11 20:32:17
+-- Dump completed on 2016-10-24 23:06:07
