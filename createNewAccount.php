1022cc2f4a2cef1a633eb18989dafd4d3dfbd82c
Hero Lee <Hero Lee>
2016-10-31 10:35:27 -0400

    Updated the database.

--- createNewAccount.php
+++ createNewAccount.php
@@ -166,10 +166,9 @@ if ($conn->query($ecomm) === TRUE) {
 	}
 	else {
 		$creditcardnumber = (int) $ccNumber;
-		$sql = "INSERT INTO user (username, firstname, lastname, email, address, city, state, ccNumber, exp, security) VALUES ('$username', '$firstname', '$lastname', '$email', '$address', '$city', '$state', '" . $creditcardnumber . "', '$exp', '$security');";
+		$sql = "INSERT INTO user (username, firstname, lastname, email, address, city, state, ccNumber, exp, security) VALUES ('$username', '$firstname', '$lastname', '$email', '$address', '$city', '$state', '$ccNumber', '$exp', '$security');";
 		if ($conn->query($sql) === TRUE) {
 			echo "You have been registered! Welcome aboard, $firstname!";
-			echo $creditcardnumber;
 		}
 		else {
 			echo "Could not insert the data." . $conn->error;
