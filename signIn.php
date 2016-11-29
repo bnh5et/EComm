bdfa56bbeb85edbde15d5856d413528635189bbd
Hero Lee <Hero Lee>
2016-11-28 22:17:28 -0500

    Added some code that is supposed to create a user session, but for some reason it's not recognizing the form attributes' names. Does anyone know why?

--- signIn.php
+++ signIn.php
@@ -1,5 +1,5 @@
 <?php
-
+session_start();
 $servername = "localhost";
 $user_name = "root";
 $password = "";
@@ -10,6 +10,22 @@ if (mysqli_connect_errno()) {
     echo "Failed to connect to MySQL: " . mysqli_connect_error();
 }
 
-
+$checkusername = $_POST['checkUsername'];
+$checkpassword = $_POST['checkPassword'];
+	//Form the SQL query
+if (!mysqli_query($conn, "SELECT * FROM user WHERE username='$checkusername' AND password = '$checkpassword'"))
+{
+    echo "Username or password not found";
+}
+else{
+	$row = $result->fetch_assoc();
+	echo "Welcome, $checkusername! You have successfully connected to MySQL! Sadly, nothing else
+	has actually been implemented yet!";
+	$_SESSION['username'] = $checkusername;
+	//$_SESSION['type'] = $row['user_type'];
+	$_SESSION['password'] = $checkpassword;
+	$_SESSION['loggedin'] = true;
+	header('Location: '."home.php");
+}
 
 ?>
