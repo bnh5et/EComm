80d3eb1d45b7676598f70605a8f3aec0bfbd3cda
brianahart <bnh5et@virginia.edu>
2016-11-29 16:06:11 -0500

    Fixed sign-in bug

--- signIn.php
+++ signIn.php
@@ -5,6 +5,7 @@ $user_name = "root";
 $password = "";
 
 $conn = mysqli_connect($servername,$user_name,$password,"ecomm");
+
 // Check connection
 if (mysqli_connect_errno()) {
     echo "Failed to connect to MySQL: " . mysqli_connect_error();
