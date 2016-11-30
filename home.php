3aa72a186fd70772ed2fdc77b2e98be3b50c4ab2
Hero Lee <Hero Lee>
2016-11-30 11:38:41 -0500

    Fixed security flaw.

--- home.php
+++ home.php
@@ -15,6 +15,9 @@
 <body>
 <?php 
 	session_start();
+        if (!isset($_SESSION['loggedin']) || $_SESSION['loggedin'] === false ) {
+            header('Location: '."index.php");
+    }
 	?>
 <div class="background">
     <div class="main-panel">
